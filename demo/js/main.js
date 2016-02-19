3006192e732e9f308b1e937cbf9c2aec78532738
Pia Henrietta <piahenrietta@wili-mbp17.local>
2016-02-19 15:32:30 +0200

    inital commit

---
+++ demo/js/main.js
@@ -0,0 +1,86 @@
+var scene, camera, renderer;
+
+var WIDTH  = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var SPEED = 0.01;
+
+function init() {
+    scene = new THREE.Scene();
+
+    initMesh();
+    initCamera();
+    initLights();
+    initRenderer();
+
+    document.body.appendChild(renderer.domElement);
+}
+
+function initCamera() {
+    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+    camera.position.set(0, 0, 5);
+    camera.lookAt(scene.position);
+}
+
+
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(WIDTH, HEIGHT);
+}
+
+function initLights() {
+    var light = new THREE.AmbientLight(0xffffff);
+    scene.add(light);
+}
+
+var mesh = null;
+function initMesh() {
+    var loader = new THREE.JSONLoader();
+    loader.load('./otso.json', function(geometry, materials) {
+        //mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+        //mesh.scale.x = mesh.scale.y = mesh.scale.z = 1;
+        //mesh.translation = THREE.GeometryUtils.center(geometry);
+        //scene.add(mesh);
+
+        /// material
+        var material = new THREE.MeshPhongMaterial( {
+            color: 0xffffff, 
+            shading: THREE.FlatShading,
+            polygonOffset: true,
+            polygonOffsetFactor: 1, // positive value pushes polygon further away
+            polygonOffsetUnits: 1,
+            wireframe: true
+        } );
+        
+        // mesh
+        mesh = new THREE.Mesh( geometry, material );
+        mesh.translation = geometry.center(geometry);
+        scene.add( mesh );
+
+        // wireframe
+        //var helper = new THREE.EdgesHelper( mesh, 0xffffff );
+        //var helper = new THREE.WireframeHelper( mesh, 0xffffff ); // alternate
+        //helper.material.linewidth = 2;
+        //scene.add( helper );
+
+    });
+}
+
+function rotateMesh() {
+    if (!mesh) {
+        return;
+    }
+
+    //mesh.rotation.x -= SPEED * 2;
+    mesh.rotation.y -= SPEED;
+    //mesh.rotation.z -= SPEED * 3;
+}
+
+function render() {
+    requestAnimationFrame(render);
+    rotateMesh();
+    renderer.render(scene, camera);
+}
+
+init();
+render();
